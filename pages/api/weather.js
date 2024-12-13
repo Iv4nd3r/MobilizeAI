baf2cc890187bb620ca5430d04c76ed32d1db3b9
Iv4nd3r <ivander.andreas@ui.ac.id>
2024-12-13 14:12:02 +0700

    refactor: Update weather API endpoint URL

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -2,7 +2,7 @@ export const fetchWeatherData = async (lat, lon, types) => {
   if (types === 'current') {
     try {
       const response = await fetch(
-        `https://server-one-clover.vercel.app/fetchCurrentWeather?lat=${lat}&lon=${lon}`
+        `https://server-one-clover.vercel.app/api/fetchCurrentWeather?lat=${lat}&lon=${lon}`
       )
       if (!response.ok) {
         throw new Error('Unable to fetch weather data, please try again')
